0eb6d05857618482779c32d99fc51438dcc1a1ad
belccarvalho <belccarvalho@gmail.com>
2021-01-27 19:22:03 +0100

    add link to footer

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+export default function footer() {
+  return (
+    <div>
+      <footer>
+        <p>
+          Made by Isabel Costa{" "}
+          <a href='https://github.com/belcosta' target='_blank'>
+            <FaGithub />
+          </a>{" "}
+          <a
+            href='https://www.linkedin.com/in/belcosta-webdeveloper/'
+            target='_blank'
+          >
+            <FaLinkedin />
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
