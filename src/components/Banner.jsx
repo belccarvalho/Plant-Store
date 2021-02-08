5a621386521d4570c547f96bcfecc2ffacc8c3bb
belccarvalho <belccarvalho@gmail.com>
2021-02-08 18:18:38 +0100

    addjust responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Banner(props) {
   return (
-    <div className='container cardEmpty '>
+    <div className='container cartEmpty '>
       <h2>{props.text}</h2>
       <Link className='goShop' to='/shop'>
         Go Shopping →
