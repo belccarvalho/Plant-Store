32829410fc7fe039c8c2af0b286305709e361909
belccarvalho <belccarvalho@gmail.com>
2021-03-07 14:00:40 +0100

    spelling correctio

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Buy() {
   return (
     <div className='buy'>
-      <h2>Working on it..</h2>
+      <h2>Working on it...</h2>
     </div>
   );
 }
